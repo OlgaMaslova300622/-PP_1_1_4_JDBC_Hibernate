11c03c3cd18bddbd0949931a4d668248eb8373ff
OlgaMaslova300622 <108478006+OlgaMaslova300622@users.noreply.github.com>
2022-09-03 17:59:24 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,33 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
+
+    private final static UserService userService = new UserServiceImpl();
+
+
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+      //  Util util = new Util();
+      //  util.getConnection();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Алексей", "Владыкин", (byte) 37);
+        userService.saveUser("Заур", "Трегулов", (byte) 35);
+        userService.saveUser("Герман", "Севостьянов", (byte) 26);
+        userService.saveUser("Наиль", "Алишев", (byte) 26);
+
+        userService.removeUserById(1);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
