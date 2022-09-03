87b378dd593e8c0ee2db1c1cebe95d42e5d65595
OlgaMaslova300622 <108478006+OlgaMaslova300622@users.noreply.github.com>
2022-09-03 17:52:45 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,106 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private final Connection  connection = Util.getConnection();
+
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS new_schema.users" + "(id BIGINT PRIMARY KEY  AUTO_INCREMENT, name VARCHAR(45), lastName VARCHAR(45), age INT)");
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     public void dropUsersTable() {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS new_schema.users");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO new_schema.users (name, lastName, age) VALUES (?,?,?)")) {
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
     }
 
     public void removeUserById(long id) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM new_schema.users WHERE id = ?")) {
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+
+        List<User> users = new ArrayList<>();
+       try (ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM new_schema.users")) {
+           while (resultSet.next()) {
+               User user = new User(resultSet.getString("name"), resultSet.getString("lastName"), resultSet.getByte("age"));
+              user.setId(resultSet.getLong("id"));
+              users.add(user);
+
+                }
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            connection.setAutoCommit(false);
+            statement.executeUpdate("TRUNCATE TABLE new_schema.users");
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
