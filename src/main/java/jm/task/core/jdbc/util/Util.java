275afc3d20b5a3fb63ada7f399e2bb01da49a4e7
OlgaMaslova300622 <108478006+OlgaMaslova300622@users.noreply.github.com>
2022-09-08 15:58:23 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.hibernate.resource.beans.container.spi.FallbackContainedBean;
+import org.hibernate.service.ServiceRegistry;
 
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -26,6 +34,42 @@ public class Util {
               System.out.println("ERROR");
           }
           return connection;
+      }
+
+     private static SessionFactory sessionFactory;
+
+      private Util() {};
+      public static SessionFactory getSessionFactory() {
+          if (sessionFactory == null) {
+              try {
+                  Configuration configuration = new Configuration();
+                  Properties settings = new Properties();
+                  settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                  settings.put(Environment.URL, "jdbc:mysql://localhost:3306/new_schema?serverTimezone=Europe/Moscow");
+                  settings.put(Environment.USER, "Olga");
+                  settings.put(Environment.PASS, "root");
+                  settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+                  settings.put(Environment.SHOW_SQL, "false");
+                  settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                  settings.put(Environment.HBM2DDL_AUTO, "update");
+
+                  configuration.setProperties(settings);
+
+                  configuration.addAnnotatedClass(User.class);
+                  ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                          .applySettings(configuration.getProperties()).build();
+
+                  sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+
+                  System.out.println("Соединение установлено1");
+
+              } catch (Exception e) {
+                  e.printStackTrace();
+                  System.out.println("ERROR1");
+              }
+          }
+          return sessionFactory;
       }
 
 
